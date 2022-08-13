24d885d8a610b780ea5f234cf5de9966589adc41
Matheus Mendonça <matheus.siquara99@gmail.com>
2022-08-13 19:48:41 -0300

    removing code

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,24 +1,5 @@
-const RESULT = document.getElementById("result");
-const BUTTONS = document.getElementsByTagName("button");
 
-const BTN_C = BUTTONS[0];
-const BTN_DELETE = BUTTONS[1];
-const BTN_DIVIDE = BUTTONS[2];
-const BTN_MULTIPLY = BUTTONS[3];
-const BTN_7 = BUTTONS[4];
-const BTN_8 = BUTTONS[5];
-const BTN_9 = BUTTONS[6];
-const BTN_MINUS = BUTTONS[7];
-const BTN_4 = BUTTONS[8];
-const BTN_5 = BUTTONS[9];
-const BTN_6 = BUTTONS[10];
-const BTN_PLUS = BUTTONS[11];
-const BTN_1 = BUTTONS[12];
-const BTN_2 = BUTTONS[13];
-const BTN_3 = BUTTONS[14];
-const BTN_EQUAL = BUTTONS[15];
-const BTN_0 = BUTTONS[16];
-const BTN_DOT = BUTTONS[17];
+const RESULT = document.getElementById("result");
 
 
 function insert(str) {
